07ddc1d48af7ccda911809d668b788e44261f886
Yasirjokhio <162189216+Yasirjokhio@users.noreply.github.com>
2024-03-07 00:05:55 +0500

    Second Work
    Assignments Work

--- main.ts
+++ main.ts
@@ -1,26 +1,9 @@
-//Let my_name = "Sahil"
+// Variable Declaration
+let myName;
+myName = "Sahil"
+console.log(myName);
+myName = "Bilal";
+console.log(myName);
 
-//my_Name = "ali"
-//console.log(myName);
-
-//const color = "blue"
-//console.log(color);
-
-//color = "red"
-
-let name1 = "ALI"
-let name2 = "Sahil";
-
-
-
-var car = "newCar"
-var car = "oldCar"
-
-
-
-
-let my_name = "sahil" //string
-let age = "20" //number
-let isMarriws = false // boolen
-let abc = undefined //undefined
-let newvalue = null
+let age = "19"
+console.log(age)
