33a938b0068c3a15c1d575e4fac924f115ef029c
Yasirjokhio <162189216+Yasirjokhio@users.noreply.github.com>
2024-03-07 00:04:37 +0500

    First Programmic
    These Are My First Files

---
+++ main.ts
@@ -0,0 +1,26 @@
+//Let my_name = "Sahil"
+
+//my_Name = "ali"
+//console.log(myName);
+
+//const color = "blue"
+//console.log(color);
+
+//color = "red"
+
+let name1 = "ALI"
+let name2 = "Sahil";
+
+
+
+var car = "newCar"
+var car = "oldCar"
+
+
+
+
+let my_name = "sahil" //string
+let age = "20" //number
+let isMarriws = false // boolen
+let abc = undefined //undefined
+let newvalue = null
